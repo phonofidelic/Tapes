f6e333c8c8c66c713ec1e2adf5f696d073b38838
phonofidelic <chris_clemons85@hotmail.com>
2019-05-26 20:08:02 -0700

    use material-ui components in Header nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,29 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Container = styled.div`
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import Button from '@material-ui/core/Button';
+import SettingsIcon from '@material-ui/icons/Settings';
+import MicIcon from '@material-ui/icons/Mic';
+
+const Container = styled(BottomNavigation)`
 	display: flex;
 	width: 100%;
 `
 
-const Button = styled.button`
+const NavItem = styled(Link)`
 	width: 100%;
-	height: 50px;
+	// height: 50px;
 `
 
 const Header = props => {
 	return (
-		<nav>
-			<Container>
-				<Link to="/">
-					<Button>Recorder</Button>
-				</Link>
-				<Link to="/settings">
-					<Button>Settings</Button>
-				</Link>
-			</Container>
-		</nav>
-	)
+		<Container>
+			<BottomNavigationAction 
+				label="Recorder" 
+				icon={<MicIcon />}
+				style={{width: '100%'}}
+				component={NavItem}
+				to="/"
+			/>
+			<BottomNavigationAction 
+				label="Settings" 
+				icon={<SettingsIcon />} 
+				style={{width: '100%'}}
+				component={NavItem}
+				to="/settings"
+			/>
+		</Container>
+	);
 }
 
 export default Header;
