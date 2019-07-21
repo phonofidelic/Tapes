6792eecaccc03f92f785caa03964503af5b1aed2
Christopher Clemons <chris.clemons85@gmail.com>
2019-07-21 00:33:43 -0700

    fix db version upgrade

--- src/db.js
+++ src/db.js
@@ -2,7 +2,12 @@
 import Dexie from 'dexie';
 
 const db = new Dexie('Tapes');
+
 db.version(1).stores({ 
+	recordings: 'id' 
+});
+
+db.version(2).stores({ 
 	recordings: 'id, title, created' 
 });
 
