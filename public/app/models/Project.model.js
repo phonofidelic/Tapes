620126785a539eeba0a422d26e50444cbe2bfa59
Christopher Clemons <chris.clemons85@gmail.com>
2019-07-22 19:11:30 -0700

    add Project model

--- public/app/models/Project.model.js
+++ public/app/models/Project.model.js
@@ -1,17 +1,16 @@
 const uuidv4 = require('uuid/v4');
 
-class Tape {
+class Project {
 	constructor(options) {
 		this.id = uuidv4();
 		this.created = Date.now();
 		this.updated = Date.now();
-		this.recording = options.recording;
-		this.start = options.start;
-		this.end: options.end;
-		this.name = options.name || 'New Tape';
+		this.recordings = options.recordings || [];
+		this.tapes  = options.tapes || [];
+		this.title = options.title || 'New Project';
 		this.tags = options.tags || [];
 		this.description = options.description || 'No descriptoin provided';
 	}
 }
 
-module.exports = Tape;
+module.exports = Project;
